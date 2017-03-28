a7dfbbbca749751f773d79ab23d1becae9a05d89
atomita <sleeping.cait.sith+gh@gmail.com>
2017-03-28 17:30:28 +0900

    v3.0.4-patch

---
+++ assets/js/admin-helper.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(i){"use strict";i(".gianism-wrap").find("h3,caption").each(function(n,e){i(e).attr("id","index_"+n),i("#index-list").append('<li><a href="#index_'+n+'">'+i(e).text()+"</a></li>")})});
+//# sourceMappingURL=map/admin-helper.js.map
